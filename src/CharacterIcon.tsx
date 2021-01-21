1d5f10c5459932fb991f4d68979e979876403a99
Carlisle <carlisle.m@hotmail.com>
2021-01-21 23:47:35 +1000

    Added logic to check for a winner, slightly improved css

--- src/CharacterIcon.tsx
+++ src/CharacterIcon.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Image } from 'react-native'
+import redcross from './images/x.png'
 
 type AppProps = {
 	characterPortrait: any
@@ -22,17 +23,33 @@ export default class CharacterIcon extends React.Component<AppProps, AppState> {
 		}
 	}
 
+	handleClick() {
+		this.setState((prevState) => ({
+			status: !prevState.status,
+		}))
+
+		const characters = [true, true, false]
+
+		// If all characters are complete, return true
+		if (characters.every((element) => element === true)) {
+			console.log('passed')
+			return true
+		} else {
+			console.log('failed')
+		}
+	}
+
 	render() {
 		return (
-			<button
-				onClick={() =>
-					this.setState((prevState) => ({
-						status: !prevState.status,
-					}))
-				}
-			>
-				<img src={this.props.characterPortrait} alt='css-icon' />
-				<div className={this.state.status ? 'show' : 'hide'}>XXX</div>
+			<button className='CharacterButton' onClick={() => this.handleClick()}>
+				<img
+					className='CharacterPortrait'
+					src={this.props.characterPortrait}
+					alt='css-icon'
+				/>
+				<div className={this.state.status ? 'show' : 'hide'}>
+					<img className='MarkComplete' src={redcross} alt='css-icon' />
+				</div>
 				{this.state.value}
 			</button>
 		)
